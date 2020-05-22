c173ae83b02b665e25117d6fbd5ae65942e10d06
aisyah <aisyahnan@upi.edu>
2020-05-22 14:56:25 +0700

    'bismillah'

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,8 +11,8 @@ class DashboardController extends Controller
         $data['barang'] = \App\Jenis::sum('jumlah_donasi');
         $data['konfirmasi'] = \App\Donatur::where('status', 'menunggu konfirmasi')->count();
         $data['riwayat_donasi'] = \App\Donatur::count();
-        $min = \App\Penerima::selectRaw('min(jumlah_menerima)')->get();
-        $data['rekomendasi'] = \App\Penerima::where('jumlah_menerima', $min)->count();
+        $min = \App\Penerima::selectRaw('min(jumlah_menerima) as min')->get();
+        $data['rekomendasi'] = \App\Penerima::where('jumlah_menerima', $min[0]->min)->count();
         return view('dashboard.index',['data' => $data]);
     }
 }
